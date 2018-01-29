83c6d6efe99a2ad586fb0f1090884fae70f47c11
Wyrmi <35914703+Wyrmi@users.noreply.github.com>
2018-01-29 13:07:50 +0200

    Add files via upload
    Trigger enter ja trigger exit

---
+++ Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("osuma");
+		gameObject.GetComponent<Renderer> ().material.color = Color.cyan;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		gameObject.GetComponent<Renderer> ().material.color = Color.magenta;
+	}
+}
