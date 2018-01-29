2cc637a668654b160915a986f8fbea5adccb28c8
Wyrmi <35914703+Wyrmi@users.noreply.github.com>
2018-01-29 13:56:19 +0200

    Add files via upload
    Max ja min value voi vaihtaa inspectorista. Default skaala on 0-1

---
+++ health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class health : MonoBehaviour {
+
+	public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("space")) {
+			slider.value = Mathf.MoveTowards (slider.value, 1.0f, 15f);
+		} else {
+			slider.value = Mathf.MoveTowards (slider.value, 0.0f, 15f);
+		}
+	}
+}
