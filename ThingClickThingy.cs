ed33ac12d34157d898e9378a8e9ce8f4681b4940
Wyrmi <35914703+Wyrmi@users.noreply.github.com>
2018-01-29 10:58:37 +0200

    Add files via upload
    kappale vaihtaa väriä hiirestä ja spacesta

---
+++ ThingClickThingy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingClickThingy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnMouseDown(){
+		gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("space")) {
+			gameObject.GetComponent<Renderer> ().material.color = Color.magenta;
+		}
+	}
+}
