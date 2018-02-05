cdb8c5b589b8fb100be2e131eda488430bb6b5a6
Wyrmi <35914703+Wyrmi@users.noreply.github.com>
2018-02-05 12:32:49 +0200

    Add files via upload
    ääni kun trigger enter

---
+++ AudioScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+	public AudioSource musicSource;		//sceneen luotu audio source objecti
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		musicSource.Play ();
+	}
+}
